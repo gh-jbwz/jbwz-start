3b0c9a16692073d33b6f742e190345bcf9a1c3af
673677097 <673677097@qq.com>
2018-11-04 23:53:20 +0800

    增加jpaéa druid 配置

--- lemon-parent/lemon-server/src/main/java/com/jbwz/lemon/server/LemonApplication.java
+++ lemon-parent/lemon-server/src/main/java/com/jbwz/lemon/server/LemonApplication.java
@@ -2,8 +2,11 @@ package com.jbwz.lemon.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.jbwz.lemon.server.entity")
+
 public class LemonApplication {
 
     public static void main(String[] args) {
---
+++ lemon-parent/lemon-server/src/main/java/com/jbwz/lemon/server/entity/User.java
@@ -0,0 +1,11 @@
+package com.jbwz.lemon.server.entity;
+
+import lombok.Data;
+
+
+@Data
+public class User {
+    private Integer Id;
+    private String name;
+    private String sex;
+}
