dba6e5e7f5e97c03a05ddd84ae7e9efc82772970
673677097 <673677097@qq.com>
2018-11-04 18:35:43 +0800

    初始化

---
+++ lemon-parent/lemon-server/src/main/java/com/jbwz/lemon/server/LemonApplication.java
@@ -0,0 +1,12 @@
+package com.jbwz.lemon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonApplication.class, args);
+    }
+}
---
+++ soft/README.md
@@ -0,0 +1,7 @@
+# 软件版本使用
+#### mysql
+下载地址:
+使用文档:[https://dev.mysql.com/doc/refman/8.0/en/windows-installation.html]https://dev.mysql.com/doc/refman/8.0/en/windows-installation.html
+
+#### navicate
+下载地址：
