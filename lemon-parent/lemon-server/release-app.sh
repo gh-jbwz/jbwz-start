06eb862724363958f3e2657bb34e37dbd5e0b9f6
countree <673677097@qq.com>
2018-11-09 11:31:16 +0800

    docker 启动okk

---
+++ lemon-parent/lemon-server/release-app.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+baseDir=`pwd`
+appName=lemon-server
+appVersion=0.0.1
+appFullName=${appName}-${appVersion}
+sed -i 's/^ENV appName=.*/ENV appName='${appFullName}'/' ${baseDir}/release/docker/Dockerfile
+
+mvn clean package -DskipTests
+if [ -e ${appFullName} ]
+then
+echo "文件夹已存在,删除重新打包."
+rm -rf ${appFullName}
+fi
+echo "开始打包项目"
+mkdir ${baseDir}/${appFullName}
+cp ${baseDir}/target/${appFullName}.jar ${baseDir}/release/docker/jar
+cp -r ${baseDir}/release/* ${baseDir}/${appFullName}/
+
+tar czvf ${appFullName}.tar.gz ${appFullName}
+echo "打包完成:${baseDir}/${appFullName}.tar.gz"
+rm -rf ${appFullName}
--- lemon-parent/lemon-server/release/bin/start-lemon-server.sh
+++ lemon-parent/lemon-server/release/bin/start-lemon-server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 baseDir=`cd .. && pwd`
+cd ${baseDir}/docker
 sudo docker-compose stop
 sudo docker-compose rm -f
-cd ${baseDir}/docker
-sudo docker-compose up -d
+sudo docker images |grep lemon-server |awk '{ print $1}' |xargs sudo docker rmi -f
+sudo docker-compose up -d --build
