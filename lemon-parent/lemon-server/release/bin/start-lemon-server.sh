4be25aacd996ccfc7c77c841a1d9516a4109d72a
countree <673677097@qq.com>
2018-11-13 14:19:09 +0800

    修改docker静态资源路径

--- lemon-parent/lemon-server/release/bin/start-lemon-server.sh
+++ lemon-parent/lemon-server/release/bin/start-lemon-server.sh
@@ -2,6 +2,10 @@
 baseDir=`cd .. && pwd`
 cd ${baseDir}/docker
 sed -i 's/appHome=.*/appHome='${baseDir}'' ${baseDir}/docker/.env
+set -e
+regxBaseDir=${baseDir////\\/}
+echo "sed -i 's/^appHome=.*/appHome='${regxBaseDir}'/' ./.env"
+sudo sed -i 's/^appHome=.*/appHome='${regxBaseDir}'/' ./.env
 sudo docker-compose stop
 sudo docker-compose rm -f
 sudo docker images |grep lemon-server |awk '{ print $1}' |xargs sudo docker rmi -f
